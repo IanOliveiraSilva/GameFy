4810639de0826527ca2a042bd3e77a7fae43a93c
wllysses <wllyssestavares@gmail.com>
2023-11-29 17:00:25 -0300

    refactor: add verify if connection with database fail

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,20 @@
-const { Pool } = require('pg');
-const dotenv = require('dotenv');
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: `postgres://aqtuzzwb:yqtOcjwCQ9phNUXpoNTYXcj8w3bA3lNm@isabelle.db.elephantsql.com/aqtuzzwb`
+  connectionString:
+    "postgres://aqtuzzwb:yqtOcjwCQ9phNUXpoNTYXcj8w3bA3lNm@isabelle.db.elephantsql.com/aqtuzzwb",
 });
-pool.on('connect', () => {
-  console.log('Base de Dados conectado com sucesso!');
+
+pool.connect((err) => {
+  if (err) {
+    console.log("Conexão com o banco de dados falhou...");
+    return;
+  }
+  console.log("Base de Dados conectado com sucesso!");
 });
+
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };

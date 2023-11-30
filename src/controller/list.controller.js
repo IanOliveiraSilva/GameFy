0393bb319f1c362c4e99089836126961bd1ae283
IanOliveiraSilva <ianoliveira.tec@gmail.com>
2023-11-30 16:45:37 -0300

    Adding list services

--- src/controller/list.controller.js
+++ src/controller/list.controller.js
@@ -1,9 +1,5 @@
 require('dotenv').config();
 
-const axios = require("axios");
-const db = require("../config/db");
-const RAWG_API_KEY = process.env.RAWG_API_KEY;
-
 const { ListService } = require("../services/list.service");
 
 const listService = new ListService();
@@ -21,111 +17,35 @@ exports.createList = async (req, res, next) => {
     }
 };
 
-exports.getAllLists = async (req, res) => {
+exports.getAllLists = async (req, res, next) => {
     try {
         const userId = req.user.id;
 
-        const lists = await db.query(
-            `SELECT l.id,l.gameIds, l.ispublic, u.username AS user, l.name AS list_name, l.description AS list_description, l.created_at AS Created_At
-            FROM lists l
-            JOIN users u ON l.userId = u.id
-            WHERE u.id = $1
-            ORDER BY Created_at DESC;
-            `,
-            [userId]
-        );
-
-        if (lists.rows.length === 0) {
-            return res.status(400).json({
-                message: 'O usuário não possui listas'
-            });
-        }
-
-        return res.status(200).json(lists.rows);
+        const response = await listService.getAllLists({ userId });
+        return res.status(200).json(response);
     } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Internal server error' });
+        next(error);
     }
 };
 
-exports.getListById = async (req, res) => {
+exports.getListById = async (req, res, next) => {
     try {
         const id = req.params.id;
 
-        const lists = await db.query(
-            `
-            SELECT 
-            u.username AS user, 
-            l.id,
-            l.gameids,
-            l.name AS list_name,
-            l.description AS list_description, 
-            l.created_at AS Created_At,
-            l.ispublic,
-            COUNT(DISTINCT game) AS games_count
-            FROM lists l
-            JOIN users u ON l.userId = u.id
-            JOIN user_profile up ON u.id = up.userid
-            LEFT JOIN 
-            unnest(l.gameIds) AS game ON true
-            WHERE l.id = $1
-            GROUP BY u.username, l.id, l.gameIds ,l.name, l.description, l.created_at;
-            `,
-            [id]
-        );
-
-        if (lists.rows.length === 0) {
-            return res.status(404).json({
-                message: 'Não foi possível encontrar a lista com o ID fornecido.'
-            });
-        }
-
-        return res.status(200).json(lists.rows);
+        const response = await listService.getListById({ id });
+        return res.status(200).json(response);
     } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            message: 'Ocorreu um erro ao buscar a lista.',
-            error
-        });
+        next(error);
     }
 };
 
-exports.getListByUser = async (req, res) => {
+exports.getListByUser = async (req, res, next) => {
     try {
         const userProfile = req.params.userProfile.toLowerCase();
 
-        const userIdQuery = await db.query('SELECT userId FROM user_profile WHERE LOWER(userProfile) LIKE $1', [userProfile]);
-
-        if (userIdQuery.rows.length === 0) {
-            return res.status(400).json({
-                message: 'O usuário não foi encontrado'
-            });
-        }
-
-        const userId = userIdQuery.rows[0].userid;
-
-        const lists = await db.query(
-            `SELECT l.id, l.gameids, u.username AS user, l.name AS list_name, l.description AS list_description, l.created_at AS Created_At
-            FROM lists l
-            INNER JOIN users u ON l.userid = u.id
-            WHERE l.userId = $1 AND l.isPublic = true
-            ORDER BY Created_at DESC;
-            `,
-            [userId]
-        );
-
-        if (lists.rows.length === 0) {
-            return res.status(404).json({
-                message: 'Não foi possível encontrar a lista com o usuario fornecido.'
-            });
-        }
-
-        return res.status(200).json(lists.rows);
+        const response = await listService.getListByUser({ userProfile });
+        return res.status(200).json(response);
     } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            message: 'Ocorreu um erro ao buscar a lista.',
-            error
-        });
+        next(error);
     }
 };
--- src/services/list.service.js
+++ src/services/list.service.js
@@ -61,6 +61,88 @@ class ListService {
             }
         }
     }
+
+    async getAllLists({ userId }){
+        const lists = await db.query(
+            `SELECT l.id,l.gameIds, l.ispublic, u.username AS user, l.name AS list_name, l.description AS list_description, l.created_at AS Created_At
+            FROM lists l
+            JOIN users u ON l.userId = u.id
+            WHERE u.id = $1
+            ORDER BY Created_at DESC;
+            `,
+            [userId]
+        );
+
+        if (lists.rows.length === 0) {
+            return res.status(400).json({
+                message: 'O usuário não possui listas'
+            });
+        }
+
+        return lists.rows;
+    }
+
+    async getListById({ id }){
+        const lists = await db.query(
+            `
+            SELECT 
+            u.username AS user, 
+            l.id,
+            l.gameids,
+            l.name AS list_name,
+            l.description AS list_description, 
+            l.created_at AS Created_At,
+            l.ispublic,
+            COUNT(DISTINCT game) AS games_count
+            FROM lists l
+            JOIN users u ON l.userId = u.id
+            JOIN user_profile up ON u.id = up.userid
+            LEFT JOIN 
+            unnest(l.gameIds) AS game ON true
+            WHERE l.id = $1
+            GROUP BY u.username, l.id, l.gameIds ,l.name, l.description, l.created_at;
+            `,
+            [id]
+        );
+
+        if (lists.rows.length === 0) {
+            return res.status(404).json({
+                message: 'Não foi possível encontrar a lista com o ID fornecido.'
+            });
+        }
+
+        return lists.rows;
+    }
+
+    async getListByUser({ userProfile}){
+        const userIdQuery = await db.query('SELECT userId FROM user_profile WHERE LOWER(userProfile) LIKE $1', [userProfile]);
+
+        if (userIdQuery.rows.length === 0) {
+            return res.status(400).json({
+                message: 'O usuário não foi encontrado'
+            });
+        }
+
+        const userId = userIdQuery.rows[0].userid;
+
+        const lists = await db.query(
+            `SELECT l.id, l.gameids, u.username AS user, l.name AS list_name, l.description AS list_description, l.created_at AS Created_At
+            FROM lists l
+            INNER JOIN users u ON l.userid = u.id
+            WHERE l.userId = $1 AND l.isPublic = true
+            ORDER BY Created_at DESC;
+            `,
+            [userId]
+        );
+
+        if (lists.rows.length === 0) {
+            return res.status(404).json({
+                message: 'Não foi possível encontrar a lista com o usuario fornecido.'
+            });
+        }
+
+        return lists.rows;
+    }
 }
 
 module.exports = { ListService };

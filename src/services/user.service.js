663a6952f7a84e61b865ea927a01bf9a8e5a7df9
IanOliveiraSilva <ianoliveira.tec@gmail.com>
2023-12-06 14:24:35 -0300

    Commenting user service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -9,62 +9,65 @@ const userRepository = new UserRepository();
 
 class UserService {
   async signUp({ username, email, password }) {
-    // Validação dos campos de entrada
+    // Validation of input fields
     if (!username || !email || !password) {
       throw new Error("All fields are required");
     }
 
-    // Validação para saber se o email já está em uso
+    // Validation to check if the email is already in use
     const emailExists = await userRepository.findUser(email);
 
     if (emailExists.length >= 1) {
       throw new Error("Email already taken");
     }
 
-    // Validação para saber se o usaurio já está em uso
+    // Validation to check if the username is already in use
     const userExists = await userRepository.findUser(username);
 
     if (userExists.length >= 1) {
       throw new Error("User already taken");
     }
 
-    // Hash da senha antes de salvar no banco de dados
+    // Hash the password before saving it to the database
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Cria novo usuario no banco de dados
+    // Create a new user in the database
     const { rows: newUser } = await userRepository.signUp({
       username,
       email,
       hashedPassword,
     });
 
-    // Gera um token de autenticação
+    // Generate an authentication token
     const token = jwt.sign(
       { id: newUser[0].id, username: newUser[0].username },
       process.env.JWT_SECRET,
       { expiresIn: "10d" }
     );
 
+    // Return the newly created user and the authentication token
     return {
       user: newUser[0],
       token,
     };
+
   }
 
   async login({ email_or_username, password }) {
-    // Validação dos campos de entrada
+    // Validation of input fields
     if (!email_or_username || !password) {
       throw new Error("Email/username and password are required");
     }
 
-    // Consulta o usuário no banco de dados pelo email ou nome de usuário
+    // Query the user in the database based on the email or username
     const user = await userRepository.login(email_or_username);
 
+    // Check if the user with the provided email/username exists
     if (!user.rows.length) {
       throw new Error("Invalid email/username or password");
     }
 
-    // Verifica se a senha fornecida corresponde à senha no banco de dados
+    // Verify if the provided password matches the password in the database
     const isPasswordCorrect = await bcrypt.compare(
       password,
       user.rows[0].password
@@ -73,13 +76,14 @@ class UserService {
       throw new Error("Invalid email/username or password");
     }
 
-    // Gera um token de autenticação
+    // Generate an authentication token
     const token = jwt.sign(
       { id: user.rows[0].id, username: user.rows[0].username },
       process.env.JWT_SECRET,
       { expiresIn: "7d" }
     );
 
+    // Return the user information and the authentication token
     return {
       user: {
         id: user.rows[0].id,
@@ -89,16 +93,21 @@ class UserService {
       },
       token,
     };
+
   }
 
   async changePassword({ userId, newPassword }) {
+    // Hash the new password before updating it in the database
     const hashedPassword = await bcrypt.hash(newPassword, 10);
 
+    // Update the user password in the database
     await userRepository.changePassword({ userId, hashedPassword });
 
+    // Return a success message
     return {
-      message: "Senha alterada com sucesso!",
+      message: "Password changed successfully!",
     };
+
   }
 
   async createUserProfile({
@@ -115,6 +124,7 @@ class UserService {
     userId,
   }) {
 
+    // Create a user profile
     const userprofile = await userRepository.createUserProfile({
       name,
       familyName,
@@ -129,59 +139,68 @@ class UserService {
       userId,
     });
 
+    // Return a success message
     return {
-      message: "Perfil criado com sucesso",
+      message: "Profile created successfully",
       profile: userprofile
     };
+
   }
 
   async getUserProfile({ userId }) {
+    // Retrieve the user profile from the database based on the provided userId
     const userProfile = await userRepository.getUserProfile({ userId })
 
-    if (userProfile == undefined) {
+    // Check if the user profile is undefined
+    if (userProfile === undefined) {
+      // If undefined, create a default profile indicating that the user doesn't have a profile
       userProfile = {
         haveProfile: false,
       };
     }
 
-    // Formata a data de nascimento para o formato 'DD/MM/AAAA'
+    // Format the birthday date to the 'DD/MM/YYYY' format
     if (userProfile && userProfile.birthday) {
-      const dataFormatada = formatarDataParaString(
-        new Date(userProfile.birthday)
-      );
-      userProfile.birthday = dataFormatada;
+      const formattedDate = formatDateToString(new Date(userProfile.birthday));
+      userProfile.birthday = formattedDate;
     }
 
+    // Return a success message along with the found user profile
     return {
-      message: "Perfil encontrado com sucesso!",
-      perfil: userProfile,
+      message: "Profile found successfully!",
+      profile: userProfile,
     };
+
   }
 
   async getProfileByUser({ userProfileParam }) {
+    // Retrieve the user profile from the database based on the provided userProfileParam
     const userProfile = await userRepository.getProfileByUser({ userProfileParam })
 
-    // Formata a data de nascimento para o formato 'DD/MM/AAAA'
+    // Format the birthday date to the 'DD/MM/YYYY' format if a profile is found
     if (userProfile && userProfile.birthday) {
-      const dataFormatada = formatarDataParaString(
-        new Date(userProfile.birthday)
-      );
-      userProfile.birthday = dataFormatada;
+      const formattedDate = formatDateToString(new Date(userProfile.birthday));
+      userProfile.birthday = formattedDate;
     }
 
+    // Return a success message along with the found user profile
     return {
-      message: "Perfil encontrado com sucesso!",
-      perfil: userProfile,
+      message: "Profile found successfully!",
+      profile: userProfile,
     };
+
   }
 
   async searchUsers({ searchQuery }) {
+    // Search for users in the database based on the provided searchQuery
     const users = await userRepository.searchUsers({ searchQuery })
 
+    // Check if no users are found
     if (!users.length) {
-      throw new Error("Nenhum usuário encontrado com a consulta fornecida");
+      throw new Error("No users found with the provided query");
     }
 
+    // Map the retrieved user data to create profiles
     const profiles = users.map((user) => {
       return {
         userId: user.userid,
@@ -194,10 +213,12 @@ class UserService {
       };
     });
 
+    // Return a success message along with the found user profiles
     return {
-      message: "Usuarios encontrados com sucesso!",
+      message: "Users found successfully!",
       users: profiles,
     };
+
   }
 
   async updateUserProfile({
@@ -211,6 +232,7 @@ class UserService {
     socialMediaTikTok,
     userId,
   }) {
+    // Update the user profile in the database with the provided information
     const newProfile = await userRepository.updateUserProfile({
       name,
       familyName,
@@ -221,12 +243,14 @@ class UserService {
       socialMediaX,
       socialMediaTikTok,
       userId,
-    })
+    });
 
+    // Return a success message along with the updated user profile
     return {
-      message: "Usuario atualizado com sucesso!",
+      message: "User updated successfully!",
       user: newProfile,
     };
+
   }
 
   async updateUserProfilePartially({
@@ -242,12 +266,15 @@ class UserService {
     userId,
   }) {
 
+    // Find the user profile in the database based on the provided userId
     const userProfile = await userRepository.findUserProfile({ userId });
 
+    // Check if the user profile is not found
     if (!userProfile) {
       throw new Error("User not found");
     }
 
+    // Create an updated profile object with the provided or existing values
     const updatedProfile = {
       name: name !== undefined ? name : userProfile.name,
       familyName: familyName !== undefined ? familyName : userProfile.familyname,
@@ -259,13 +286,16 @@ class UserService {
       socialMediaTikTok: socialMediaTikTok !== undefined ? socialMediaTikTok : userProfile.socialmediatiktok,
       icon: icon !== undefined ? icon : userProfile.icon,
     };
-  
-    const newProfile = await userRepository.updateUserProfilePartially(updatedProfile, userId)
-    
+
+    // Update the user profile in the database with the partially updated values
+    const newProfile = await userRepository.updateUserProfilePartially(updatedProfile, userId);
+
+    // Return a success message along with the updated user profile
     return {
-      message: "Usuario atualizado com sucesso!",
+      message: "User updated successfully!",
       user: newProfile,
     };
+
   }
 
   async getRatingCount({ userId }) {
